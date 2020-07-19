43606b0c3144d19a10509176ffbc7b37812dea4b
TheNear <thenearx@gmail.com>
2020-07-19 09:59:40 +0300

    add CustomInput

--- src/components/BookEditor/BookEditor.jsx
+++ src/components/BookEditor/BookEditor.jsx
@@ -5,6 +5,9 @@ function BookEditor() {
   return (
     <div className={style.wrapper}>
       <h2>Редактирование книги</h2>
+      <form>
+        
+      </form>
     </div>
   );
 }
---
+++ src/componentsHelper/Input/CustomInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CustomInput() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CustomInput;
+
