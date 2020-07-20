d27cbced2fc3a45d3ef36af04032743309052cc8
TheNear <thenearx@gmail.com>
2020-07-20 14:57:02 +0300

    Updates

---
+++ precache-manifest.55e0aa0cb8f08627e21cc2efa01ba0ad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e98889faf8ff7730e0e10655a2b39b2",
+    "url": "/Bookshelf/index.html"
+  },
+  {
+    "revision": "7893f8360be10276ec9d",
+    "url": "/Bookshelf/static/css/main.2368cf23.chunk.css"
+  },
+  {
+    "revision": "b005d440910c98afb9ff",
+    "url": "/Bookshelf/static/js/2.cb668909.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Bookshelf/static/js/2.cb668909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7893f8360be10276ec9d",
+    "url": "/Bookshelf/static/js/main.066bea37.chunk.js"
+  },
+  {
+    "revision": "90c45a6dc1a2c4745eca",
+    "url": "/Bookshelf/static/js/runtime-main.6770961f.js"
+  },
+  {
+    "revision": "4a6848ac728ad06451b221606ea5102a",
+    "url": "/Bookshelf/static/media/book.4a6848ac.svg"
+  },
+  {
+    "revision": "3eadce1685240a13a3ee5f9a223fbbe6",
+    "url": "/Bookshelf/static/media/cancel.3eadce16.svg"
+  },
+  {
+    "revision": "50d147293e706ba10be28f9e5465605f",
+    "url": "/Bookshelf/static/media/disk.50d14729.svg"
+  },
+  {
+    "revision": "31fbd85d3d27eb87e1c39762726836d6",
+    "url": "/Bookshelf/static/media/garbage.31fbd85d.svg"
+  },
+  {
+    "revision": "a1cd3cdb347b7d3c7dd2fd7b45529084",
+    "url": "/Bookshelf/static/media/pencil.a1cd3cdb.svg"
+  }
+]);
